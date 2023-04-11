c8e4bbd8474d8b628ddccd860cfcd4f65c42a167
bscwebmaster <56090048+bscwebmaster@users.noreply.github.com>
2023-04-11 10:05:45 -0700

    Added item to url-list, testing filename creation

--- soup-write-pages-from-list-to-files.py
+++ soup-write-pages-from-list-to-files.py
@@ -6,7 +6,17 @@ from bs4 import BeautifulSoup
 myurls = open('url-list.txt').read().splitlines()
 # loop through that list
 for myurl in myurls:
+    # first get the url
     r = requests.get(myurl)
+    # testing: print the url
+    print(os.path.relpath(myurl))
+    # split the url into bits
+    mypath = myurl.split(os.sep)
+    # delete https: and domain
+    del mypath[0]
+    del mypath[1]
+    myfn = 'bsc.coop' + '_'.join(mypath) + ".html"
+    print(myfn)
     soup = BeautifulSoup(r.content, features="html5lib")
     fn = os.path.basename(myurl) + ".html"
     f = open(fn, "w")
