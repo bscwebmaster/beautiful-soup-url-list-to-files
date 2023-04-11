986e329bc5f268b40192d839b5a57bd3797f8129
bscwebmaster <56090048+bscwebmaster@users.noreply.github.com>
2023-04-11 08:56:11 -0700

    added .html to filename, changed url-list filename

--- soup-write-pages-from-list-to-files.py
+++ soup-write-pages-from-list-to-files.py
@@ -3,12 +3,12 @@ import os.path
 import requests
 from bs4 import BeautifulSoup
 # open file, create the list of URLs
-myurls = open('sitemap.txt').read().splitlines()
+myurls = open('url-list.txt').read().splitlines()
 # loop through that list
 for myurl in myurls:
     r = requests.get(myurl)
     soup = BeautifulSoup(r.content)
-    fn = os.path.basename(myurl)
+    fn = os.path.basename(myurl) + ".html"
     f = open(fn, "w")
     f.write(soup.prettify())
     f.close
