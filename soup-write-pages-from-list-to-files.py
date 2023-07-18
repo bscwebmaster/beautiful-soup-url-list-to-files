b842133b711326fd50a5e1dbd891a6dfa68df54b
bscwebmaster <56090048+bscwebmaster@users.noreply.github.com>
2023-07-18 14:51:41 -0700

    changed some variable names, added folder name to output path

--- soup-write-pages-from-list-to-files.py
+++ soup-write-pages-from-list-to-files.py
@@ -5,18 +5,18 @@ import requests
 from bs4 import BeautifulSoup
 from def_list import *
 # open file, create the list of URLs
-myurls = open('url-list.txt').read().splitlines()
+myurls = open('html/url-list.txt').read().splitlines()
 # loop through that list
 for myurl in myurls:
     # first get the url
     R = requests.get(myurl)
     # split the url into bits
-    MYPATH = myurl.split(os.sep)
+    myname = myurl.split(os.sep)
     # delete https: and domain
-    del MYPATH[0]
-    del MYPATH[1]
+    del myname[0]
+    del myname[1]
     # concatenate filename
-    MYFN = 'bsc.coop' + '_'.join(MYPATH) + ".html"
+    MYFN = 'html/bsc.coop' + '_'.join(myname) + ".html"
     # make soup
     soup = BeautifulSoup(R.content, features="html5lib")
 
